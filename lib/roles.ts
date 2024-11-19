249225131665f31fd196ab5e2b629d4d6a3491d4
danny-fuhriman_atko <danny.fuhriman@okta.com>
2024-11-18 21:27:26 -0800

    fix types

--- lib/roles.ts
+++ lib/roles.ts
@@ -10,9 +10,14 @@ export const roles = {
 export type Role = keyof typeof roles
 
 export function getRole(user: UserProfile) {
+  console.log(user)
   // we only allow a single role to be assigned to a user
-  const role = (user[ROLES_CLAIM_KEY] as string[])[0]
+  const roles = user[ROLES_CLAIM_KEY] as string[]
+
+  if (Array.isArray(roles) && roles.length > 0) {
+    return roles[0]
+  }
 
   // if no role is assigned, set them to the default member role
-  return role || "member"
+  return "member"
 }
